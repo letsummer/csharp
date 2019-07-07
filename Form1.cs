d2b7810f4d18d2b3e676ba9710bd601dacb4adb1
letsummer <46320318+letsummer@users.noreply.github.com>
2019-07-07 22:17:43 +0900

    계산기프로그램
    190707 최초생성

---
+++ Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        //int one, two, three, four, five, six, seven, eight, nine, zero = 0;
+        //double sum=0.0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "1";
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "2";
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "3";
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "4";
+
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "5";
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "6";
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "7";
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "8";
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "9";
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "0";
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "+";
+        }
+
+        private void btnSub_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "-";
+        }
+
+        private void btnMul_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "*";
+        }
+
+        private void btnDiv_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "/";
+        }
+
+        private void btnSum_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAc_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+    }
+}
